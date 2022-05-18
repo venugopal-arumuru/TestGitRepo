11f1e261f06e833703dfd6032a912d62a3bbc1fe
venugopal <Venugopal Arumuru@LAPTOP-26A7CNVA>
2022-05-18 10:36:08 +0530

    new java is added...

--- TestProject/src/com/java/test/Test1.java
+++ TestProject/src/com/java/test/Test1.java
@@ -4,6 +4,10 @@ public class Test1 {
 // test app for git
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		int x = 100;
+		System.out.println("X value is : " + x);
+		float y = 12.45f;
+		System.out.println("Y value is : " + y);
 	}
 
 }
---
+++ TestProject/src/com/java/test/Test2.java
@@ -0,0 +1,9 @@
+package com.java.test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is new second file");
+	}
+
+}
